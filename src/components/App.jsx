86f5bdeb6595e485812e4785e4935379bb53cbaa
Alex J <abel.oalex@gmail.com>
2020-01-17 13:09:14 -0300

    added basic input

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ToDoList from "./TodoList";
+import TodoInputComponent from "./todoInput";
 
 const App = () => {
     return <div>
         <h1>A React Application</h1>
         < ToDoList />
+        < TodoInputComponent />
     </div>
 };
 
